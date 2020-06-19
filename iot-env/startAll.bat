16e4cd528175692928adfb4e46578e5e5f09df49
Fradantim <Fradantim@host.docker.internal>
2020-06-19 02:03:44 -0300

    asd

--- iot-env/startAll.bat
+++ iot-env/startAll.bat
@@ -6,6 +6,4 @@ start java -jar iot-env-filterServer\target\iot-env-filterServer.war --server.po
 start java -jar iot-env-windowActuator\target\iot-env-windowActuator.war --server.port=8082
 start java -jar iot-env-windowSensor\target\iot-env-windowSensor.war --server.port=8083
 start java -jar iot-env-thermometerSensor\target\iot-env-thermometerSensor.war --server.port=8084
-
-PING localhost -n 4 > NUL
 start java -jar iot-env-heatingServer\target\iot-env-heatingServer.war --server.port=8085
